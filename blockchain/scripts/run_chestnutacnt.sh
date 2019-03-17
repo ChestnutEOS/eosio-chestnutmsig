ae57996cc50c8d42579f4d8e0e4084bc562b7ca4
JackDiSalvatore <jdisalvatore7@gmail.com>
2019-03-17 10:20:01 -0400

    cleaning up the test script and explicitly testing the permission scheme

--- blockchain/scripts/run_chestnutacnt.sh
+++ blockchain/scripts/run_chestnutacnt.sh
@@ -7,45 +7,46 @@ echo '==================================================================='
 echo 'Give daniel an initial EOS balance of 1000.0000 EOS'
 cleos push action eosio.token transfer '[ "eosio","daniel","1000000000.0000 EOS", "starting balance" ]' -p eosio eosio.token; sleep 1
 
-echo 'get account chestnutacnt'
-cleos get account chestnutacnt
-
-echo '=== New User daniel ==='
+echo '============================================================'
+echo '=== New User daniel turns eos account into smart account ==='
 ## This needs to all be done external to the smart contract
-echo '=== MODIFY PERMISSIONS  ==='
-echo 'temporarily give chestnutacnt@eosio.code access to daniel@active'
-
+echo '===                    MODIFY  PERMISSIONS               ==='
+echo '============================================================'
+echo 'Give chestnutacnt@eosio.code access to daniel@active'
 cleos push action eosio updateauth '{"account":"daniel","permission":"active","parent":"owner","auth":{"keys":[{"key":"EOS6kYgMTCh1iqpq9XGNQbEi8Q6k5GujefN9DSs55dcjVyFAq7B6b", "weight":1}],"threshold":1,"accounts":[{"permission":{"actor":"chestnutacnt","permission":"eosio.code"},"weight":1}],"waits":[]}}' -p daniel
-
 sleep 1
-
-# DO IT HERE
+echo 'Create the smart account'
 echo 'cleos push action chestnutacnt reg ["daniel","EOS8GKM..."] -p daniel@active'
 cleos push action chestnutacnt reg '["daniel","EOS8GKMDqyr9MveUE7RKx11vj2HfS3sMqzn97QtDXd2Fo9X87iB39"]' -p daniel@active
-
 sleep 1
 
-# echo 'remove chestnutacnt@eosio.code from daniel@active'
-# cleos set account permission daniel active \
-# '{"threshold": 1,"keys": [{"key": "EOS6kYgMTCh1iqpq9XGNQbEi8Q6k5GujefN9DSs55dcjVyFAq7B6b","weight": 1}],"accounts": [], "waits":[]}' \
-# owner -p daniel
-
-sleep 1
-
-
+echo '======================================================'
+echo '===                    ACCOUNTS                    ==='
+echo '======================================================'
+echo 'cleos get account chestnutacnt'
+cleos get account chestnutacnt
 echo 'cleos get account daniel'
 cleos get account daniel
 
 sleep 1
-# echo 'make sure normal transfers do not work'
-# echo 'cleos push action eosio.token transfer ["daniel","chestnutacnt","10.0000 EOS","memo"] -p daniel@chesnut'
-# cleos push action eosio.token transfer '["daniel","chestnutacnt","10.0000 EOS","memo"]' -p daniel@chestnut
-# cleos push action eosio.token transfer '["chestnutacnt","daniel","10.0000 EOS","memo"]' -p chestnutacnt'
-# echo 'make sure other actions do not work'
-# echo 'cleos push action chestnutacnt hello '
-# cleos push action chestnutacnt hello '[""]' -p daniel@chestnut
+echo 'Make sure normal transfers still work with the @active permission'
+echo 'cleos push action eosio.token transfer ["daniel","chestnutacnt","10.0000 EOS","memo"] -p daniel@active'
+cleos push action eosio.token transfer '["daniel","chestnutacnt","10.0000 EOS","memo"]' -p daniel@active
+cleos push action eosio.token transfer '["chestnutacnt","daniel","10.0000 EOS","memo"]' -p chestnutacnt
+sleep 1
+echo 'Make sure smart transfers do NOT work with the @active permission or @chestnut permission'
+echo 'cleos push action eosio.token transfer ["daniel","chestnutacnt","10.0000 EOS","memo"] -p daniel@active'
+cleos push action chestnutacnt transfer '["daniel","chestnutacnt","10.0000 EOS","memo"]' -p daniel@active
+echo 'cleos push action eosio.token transfer ["daniel","chestnutacnt","10.0000 EOS","memo"] -p daniel@chestnut'
+cleos push action chestnutacnt transfer '["daniel","chestnutacnt","10.0000 EOS","memo"]' -p daniel@chestnut
+sleep 1
+echo 'Make sure other actions can not be called with the @chestnut permission'
+echo 'cleos push action chestnutacnt hello'
+cleos push action chestnutacnt hello '[""]' -p daniel@chestnut
 
-echo '=== ADD NEW EOS TRANSFER LIMIT ==='
+echo '========================================================'
+echo '===             ADD NEW EOS TRANSFER LIMIT           ==='
+echo '========================================================'
 echo 'daniel adds a new EOS token spending limit'
 echo 'he does not wish to send more than 100 EOS per transfer'
 echo 'cleos push action chestnutacnt addtokenmax ["daniel","100.0000 EOS","eosio.token"] -p daniel@chestnut'
@@ -54,7 +55,7 @@ sleep 1
 echo 'cleos get table chestnutacnt daniel tokensmax'
 cleos get table chestnutacnt daniel tokensmax
 
-echo '=== daniel transfers 99 EOS through chestnutacnt to sally ==='
+echo '`daniel` transfers 99 EOS through chestnutacnt to sally'
 # echo 'this should fail'
 # echo 'cleos push action chestnutacnt transfer ["daniel","sally","100.0001 EOS","finding memo"] -p daniel@chestnut'
 # cleos push action chestnutacnt transfer '["daniel","sally","100.0001 EOS","finding memo"]' -p daniel@chestnut
@@ -75,7 +76,9 @@ cleos get table eosio.token daniel accounts
 echo 'cleos get table eosio.token sally accounts'
 cleos get table eosio.token sally accounts
 
-echo '=== CLEAN UP ==='
+echo '============================================='
+echo '===               CLEAN UP                ==='
+echo '============================================='
 sleep 1
 echo 'cleos push action chestnutacnt rmtokenmax ["daniel","4,EOS"] -p daniel@chestnut'
 cleos push action chestnutacnt rmtokenmax '["daniel","4,EOS"]' -p daniel@chestnut
