ff6e3e30a14dd86d91b40ec28027a111b5c91190
JackDiSalvatore <jdisalvatore7@gmail.com>
2019-03-16 20:53:48 -0400

    removing the tranfser permission and using the active permission instead.  Also removing the users public from the active permission and taking it over with chestnutacnt@eosio.code

--- blockchain/contracts/chestnutacnt/chestnutacnt.cpp
+++ blockchain/contracts/chestnutacnt/chestnutacnt.cpp
@@ -15,10 +15,10 @@ void chestnutacnt::reg( name user,
    require_auth( user );
 
    set_auth_with_key( user, "chestnut"_n, "active"_n, chestnut_public_key );
-   set_auth_with_code( user, "transfer"_n, "active"_n, CONTRACT_ACCOUNT,
+   set_auth_with_code( user, "active"_n, "owner"_n, CONTRACT_ACCOUNT,
                                                        "eosio.code"_n );
 
-   // link auth of user@config to
+   // link auth of user@chestnut to
    //   config::addtokenmax
    //   config::rmtokenmax
    name action_names[3] = { "addtokenmax"_n, "rmtokenmax"_n, "transfer"_n };
@@ -60,17 +60,6 @@ void chestnutacnt::addtokenmax( name  user,
         });
    }
 
-   // link auth of user@transfer to contract_account::transfer
-   action(
-      permission_level{ user, "active"_n },
-      "eosio"_n,
-      "linkauth"_n,
-      std::make_tuple( user,
-                       token_max_itr->contract_account,
-                       "transfer"_n,
-                       "transfer"_n )
-   ).send();
-
 }
 
 
@@ -83,16 +72,6 @@ void chestnutacnt::rmtokenmax( name user, symbol sym ) {
 
    eosio_assert( token_max_to_delete != token_max_table.end(),
                  "can not find token max to delete" );
-
-   // unlink auth of user@transfer to contract_account::transfer
-   action(
-      permission_level{ user, "active"_n },
-      "eosio"_n,
-      "unlinkauth"_n,
-      std::make_tuple( user,
-                       token_max_to_delete->contract_account,
-                       "transfer"_n )
-   ).send();
 
    token_max_table.erase( token_max_to_delete );
 }
@@ -118,7 +97,7 @@ void chestnutacnt::transfer( name      from,
    /****/
 
    action(
-      permission_level{ from, "transfer"_n },
+      permission_level{ from, "active"_n },
       token_max_itr->contract_account,
       "transfer"_n,
       std::make_tuple( from, to, quantity, memo )
